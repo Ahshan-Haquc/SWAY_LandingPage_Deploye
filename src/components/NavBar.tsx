1c1472e87931972c5282902346413f622012e8c6
akashmuhammadabrrar <madborakash48@gmail.com>
2025-12-15 18:02:06 +0600

    this commit for the existing design improved and a little bit of remaining

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,29 +18,25 @@ const ChatIcon = () => {
       {showIcon && (
         <div className="fixed top-4 md:top-5 left-1/2 -translate-x-1/2 z-100 bg-black/40 backdrop-blur-[10px] px-1 py-1 md:py-1 rounded-full flex items-center shadow-lg text-nowrap text-white">
           <Link href="/" className="hidden md:flex h-12 w-12  items-center rounded-full duration-300 hover:bg-[#FF6464]">
-          <div className=" h-8 w-8 relative filter brightness-0 invert mx-4  ">
-          <div
-          
-          >
-            <Image alt="Y" src={logo} fill className="h-full w-full object-contain "/>
-          </div>
-          </div>
+            <div className="h-8 w-8 relative filter brightness-0 invert mx-4  ">
+              <div>
+                <Image alt="Y" src={logo} fill className="h-full w-full object-contain " />
+              </div>
+            </div>
           </Link>
 
           <Link
             href="/contact"
-            className={`text-white  text-xs md:text-sm px-2 md:px-4 py-4 rounded-full transition duration-300 ${
-              isActive("/contact") ? "bg-[#FF6464] hover:bg-[#FF6464] text-black hover:text-white": "hover:bg-[#FF6464]"
-            }`}
+            className={`text-white  text-xs md:text-sm px-2 md:px-4 py-4 rounded-full transition duration-300 ${isActive("/contact") ? "bg-[#FF6464] hover:bg-[#FF6464] text-black hover:text-white" : "hover:bg-[#FF6464]"
+              }`}
           >
             CONTACT US
           </Link>
 
           <Link
             href="/freeConsultation"
-            className={`text-white text-xs md:text-sm px-2 md:px-4 py-4 rounded-full transition duration-400 ${
-              isActive("/freeConsultation") ? "bg-[#FF6464] hover:bg-[#FF6464] text-black hover:text-white": "hover:bg-[#FF6464]"
-            }`}
+            className={`text-white text-xs md:text-sm px-2 md:px-4 py-4 rounded-full transition duration-400 ${isActive("/freeConsultation") ? "bg-[#FF6464] hover:bg-[#FF6464] text-black hover:text-white" : "hover:bg-[#FF6464]"
+              }`}
           >
             FREE CONSULTATION
           </Link>
